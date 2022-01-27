7c41e7aed9edcce10ecd18d81cea59c580d62780
king <jinch1995@outlook.com>
2022-01-27 22:48:20 +0800

    新增了HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-27 22:29:53
+ * @LastEditTime: 2022-01-27 22:31:46
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue3c:\code\test\HelloWorld.java
+ */
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
